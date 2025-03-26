ca7bc24ae29c618c2f37c597407c5232d94eda22
Nafis Hossain <103623207+NafisHoss04@users.noreply.github.com>
2025-03-26 16:40:19 +0000

    Used require instead of import

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import core from '@actions/core';
-import github from '@actions/github';
+const core = require('@actions/core');
 
 const name = core.getInput('name');
 const output = `Hello ${name}`;
